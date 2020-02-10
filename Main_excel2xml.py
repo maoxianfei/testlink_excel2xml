52a907b043f028aa1e7e81ec6940db8d18c14af9
richard <richard.mao@kucoin.com>
2020-02-10 22:12:49 +0800

    增加主程序

---
+++ Main_excel2xml.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+@File Name：Main_excel2xml
+@Software: PyCharm
+@Author : MaoXianfei
+@Time：2020/2/10 12:13 上午 
+"""
+from excelParse import ExcelParser
+from excel2xml import operate
+
+def Control(file):
+    sheetnames=ExcelParser(file).get_sheet_name()
+    for sheetname in sheetnames:
+        print(f"{sheetname} - 模块完成")
+        op = operate(file, sheetname)
+        op.dic_to_xml()
+if __name__ == '__main__':
+    # 一个sheet表输出一个xml文件
+    file="发奖系统.xlsx"
+    Control(file)
